273cc510902d6ef5928e9f88b1137bbb6e2c930f
jvmfdev <72178115+jvmfdev@users.noreply.github.com>
2021-02-19 05:20:36 -0600

    OAuth with Passport and Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '834320770448921',
+        clientSecret: 'e7f8dc7848b797a48f3e2783ab1a848e'
+    }
 }
